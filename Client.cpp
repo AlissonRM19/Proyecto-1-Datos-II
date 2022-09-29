73eee0fe273c4137d730a8881a07e3dce524e7ae
AlissonRM19 <88214936+AlissonRM19@users.noreply.github.com>
2022-09-28 22:31:09 -0600

    Creacion de funcion para la divicion de la imagen

--- Client.cpp
+++ Client.cpp
@@ -1,8 +1,96 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
 
+using namespace cv;
 using namespace std;
+
+/*
+ * image divider
+ */
+int divideImage(const cv::Mat& img, const int blockWidth, const int blockHeight, std::vector<cv::Mat>& blocks)
+{
+    // Checking if the image was passed correctly
+    if (!img.data || img.empty())
+    {
+        std::wcout << "Image Error: Cannot load image to divide." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // init image dimensions
+    int imgWidth = img.cols;
+    int imgHeight = img.rows;
+    std::wcout << "IMAGE SIZE: " << "(" << imgWidth << "," << imgHeight << ")" << std::endl;
+
+    // init block dimensions
+    int bwSize;
+    int bhSize;
+
+    int y0 = 0;
+    while (y0 < imgHeight)
+    {
+        // compute the block height
+        bhSize = ((y0 + blockHeight) > imgHeight) * (blockHeight - (y0 + blockHeight - imgHeight)) + ((y0 + blockHeight) <= imgHeight) * blockHeight;
+
+        int x0 = 0;
+        while (x0 < imgWidth)
+        {
+            // compute the block height
+            bwSize = ((x0 + blockWidth) > imgWidth) * (blockWidth - (x0 + blockWidth - imgWidth)) + ((x0 + blockWidth) <= imgWidth) * blockWidth;
+
+            // crop block
+            blocks.push_back(img(cv::Rect(x0, y0, bwSize, bhSize)).clone());
+
+            // update x-coordinate
+            x0 = x0 + blockWidth;
+        }
+
+        // update y-coordinate
+        y0 = y0 + blockHeight;
+    }
+    return EXIT_SUCCESS;
+}
+
+
+
 int main() {
+
+    /*
+     * Image management
+     */
+    cv::Mat image = imread("Prueba.jpeg", IMREAD_COLOR);
+
+    if (image.empty()) { //Verify if the image has been readed correctly
+        cout << "Image File "
+             << "Not Found" << endl;
+        cin.get(); // wait for any key press
+        return -1;
+    }
+    imshow("ImageWindow", image);
+    waitKey(0);
+
+    /*
+     * Divide the image
+     */
+    // init vars
+    const int blockw = 128;
+    const int blockh = 128;
+    std::vector<cv::Mat> blocks;
+    int divideStatus = divideImage(image, blockw, blockh, blocks);
+
+    // debug: save blocks
+    cv::utils::fs::createDirectory("blocksFolder");
+    for (int j = 0; j < blocks.size(); j++)
+    {
+        std::string blockId = std::to_string(j);
+        std::string blockImgName = "blocksFolder/block#" + blockId + ".jpeg";
+        imwrite(blockImgName, blocks[j]);
+    }
+
+
+
+    //Client Socket
     boost::asio::io_service io_service; // Servicio de input/output
     boost::asio::ip::tcp::socket socket(io_service); // Declaracion de socket para conexiones
     boost::system::error_code error; // Variable para codigo de error especifico de Boost
